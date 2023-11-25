a4e3203c446e24468b59c45fb6712f0dee3bb49e
G4iteir00 <149211758+G4iteir00@users.noreply.github.com>
2023-11-25 03:49:20 +0000

    feat: criação da base do projeto com express

---
+++ src/routes/index.route.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const routes = (app) => {
+  app.route('/').get((req, res) => {
+    res.status(200).send({titulo: "Hello World"})
+  })
+
+  app.use(
+    express.json(),
+    // novas rotas
+  )
+}
+
+export default routes;
